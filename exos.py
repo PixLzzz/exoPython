4b388e25eabe070039e528d6cabd78f1b6659a21
PixLzzz <36031553+PixLzzz@users.noreply.github.com>
2020-05-09 15:50:11 +0200

    Update exos.py

--- exos.py
+++ exos.py
@@ -242,5 +242,24 @@ import sys
 #  if res[0] < res[1] :
 #    print(res[0])
 
-
+#EPISODE 6
+#import re
+#try:
+#    f = open('ep6.txt', mode="r")
+#    f.seek(0) 
+#    first_char = f.read(1) 
+#    if not first_char:
+#        print("file is empty")
+#    else:
+#        f.seek(0)
+#        for x in f :
+#            temp = re.findall(r'\d+', x)
+#            res = list(map(int, temp))
+#            if res[0] < res[1]:
+#                print(x ," OUI")
+#            else :
+#                print(x, " NON")
+#    f.close()
+#except FileNotFoundError:
+#    print('File does not exist')
 
